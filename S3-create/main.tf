30f09af681ac384bd573478ce1cac1e74f5cc0e7
techwhisper-in <technicalcodelab@gmail.com>
2024-08-29 19:05:33 +0530

    all

--- S3-create/main.tf
+++ S3-create/main.tf
@@ -1,6 +1,6 @@
 module "S3" {
   #count = var.infra_create=="true" ? 1 : 0
-  source     = "./modules/s3_bucket"
+  source     = "../modules/s3_bucket"
   Reg        = var.infra_region
   infra_tags = var.infra_tags
   infra_create = var.infra_create
--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "techwhisper-ami-snapshot-ebs-volume-inventory"
-    key            = "file/create-s3.tfstate"
-    region         = "ap-south-1"
-  }
-}
