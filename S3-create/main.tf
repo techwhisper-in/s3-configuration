c477b2d50326324c16b52eddec1a07b7bf823895
techwhisper-in <technicalcodelab@gmail.com>
2024-09-07 12:31:21 +0530

    add

--- S3-create/main.tf
+++ S3-create/main.tf
@@ -1,6 +1,7 @@
 module "S3" {
   #count = var.infra_create=="true" ? 1 : 0
   source     = "../modules/s3_bucket"
+  infra_account = var.infra_account
   Reg        = var.infra_region
   infra_tags = var.infra_tags
   infra_create = var.infra_create
